3a488bc9bf5132370b9a66cae5f549d99917b0f1
yakshe-mash <galardolp550@gmail.com>
2021-02-14 14:43:04 +0300

    1.5. Поля объекта [#440380]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,31 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Chernish");
+        black.show();
     }
 }
